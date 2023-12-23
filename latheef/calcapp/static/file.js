e413d82dfcea0d80c43eb41b5c9d31c78bf29bdb
VineelaShaik <vineelalatheef@gmail.com>
2023-12-23 12:11:11 +0530

    Success

---
+++ latheef/calcapp/static/file.js
@@ -0,0 +1,12 @@
+function display(value){
+    document.getElementById("result").value+=value;
+ }
+  function clear(){
+     document.getElementById("result").value='';
+  }
+  function solve(){
+    let x = document.getElementById('result').value
+    let y = eval(x);
+    document.getElementById('result').value = y
+    return y
+}
